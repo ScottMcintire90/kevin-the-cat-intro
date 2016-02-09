d79bfb866de6b944610a736ae0f0cbccb0fbcf3e
Scott <scottmcintire90@gmail.com>
2016-02-08 20:41:40 -0800

    added more attributes, inverted color scheme for profile pic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 $(document).ready(function()  {
+
   $("button#black").click(function() {
     $("body").removeClass();
     $("body").addClass("black-background");
   });
+
   $("button#white").click(function() {
     $("body").removeClass();
     $("body").addClass("white-background");
+  });
+
+  $(".clickable").click(function() {
+    $(".meow").toggle();
+    $(".nomeow").toggle();
+  });
+
+  $("#inverted").click(function() {
+    $("img").addClass(".invert");
   });
 });
